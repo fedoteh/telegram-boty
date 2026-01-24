c38d82ce256bf38bb39cc04ac9cbd6b0c4abb6a4
Fedoteh <fedebenavidez@live.com>
2026-01-24 17:44:57 -0300

    feat: gta 6 countdown on `/gta`

--- src/listeners/register-listeners.ts
+++ src/listeners/register-listeners.ts
@@ -3,9 +3,14 @@ import type { LoadedConfig } from "../config/load-config.js";
 import messageListener from "./message.js";
 import editedMessageListener from "./edited_message.js";
 import statsListener from "./stats.js";
+import gtaListener from "./gta.js";
 
 export const registerListeners = (bot: Bot, config: LoadedConfig) => {
+  // Register specific listeners first (commands, reactions, etc.)
+  gtaListener(bot);
   statsListener(bot, config);
+
+  // Register general listeners last (catch-all message handlers)
   messageListener(bot);
   editedMessageListener(bot);
 };
